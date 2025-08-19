6f3ab23d2e1eaaa8e2f42c0cda1295b02014566b
ken-fs <493129720ljw@gmail.com>
2025-08-19 23:06:21 +0800

    feat: 使用config模块获取环境配置文件

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post } from '@nestjs/common'
 import { UserService } from './user.service'
 import { ConfigService } from '@nestjs/config'
 import { ConfigEnum } from 'src/enum/config.enum'
+import config from 'config'
 @Controller('user')
 export class UserController {
   constructor(
@@ -12,9 +13,9 @@ export class UserController {
   @Get('getUser')
   getUser() {
     const db = this.configService.get<string>(ConfigEnum.DB_HOST)
-    const db2 = this.configService.get<string>('DB_HOST')
-    const url = this.configService.get<string>('DB_URL')
-    console.log('db', db, 'db2', db2, 'url', url)
+    const db2 = this.configService.get<string>('DB_HOST') // 使用nestjs/config获取环境配置文件
+    const db3 = config.get<string>('DB_URL') // 使用config模块获取环境配置文件
+    console.log('db', db, 'db2', db2, 'db3', db3)
     return this.userService.getUser()
   }
 
@@ -23,3 +24,4 @@ export class UserController {
     return this.userService.addUser()
   }
 }
+
