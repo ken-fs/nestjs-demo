83869084185d2f7d6638d01c82a10536cb5ad114
ken-fs <493129720ljw@gmail.com>
2025-08-24 16:18:04 +0800

    feat: nestjs重新删除

--- src/enum/config.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum ConfigEnum {
-  DB_HOST = 'DB_HOST',
-  DB_PORT = 'DB_PORT',
-  DB_USER = 'DB_USER',
-  DB_PASSWORD = 'DB_PASSWORD',
-  DB_DATABASE = 'DB_DATABASE',
-  DB_URL = 'DB_URL',
-}
--- src/range/range.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { RangeService } from './range.service'
-import { RangeController } from './range.controller'
-
-@Module({
-  providers: [RangeService],
-  controllers: [RangeController],
-})
-export class RangeModule {}
